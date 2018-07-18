96bddccd1cebfe7bf7812f2a5a831abb7c0b59cd
Benjamin Noland <benjaminnoland93@gmail.com>
2018-07-18 15:37:06 -0400

    Divided the app over multiple files (server.R, ui.R, global.R)

---
+++ global.R
@@ -0,0 +1,28 @@
+# File for defining global data, including loading the nurses data set.
+
+library(readr)
+
+source("factor_levels.R")
+
+# Load the data -------------------------------------------------------------------------------
+
+nurses <- read_csv("nurses_preprocessed.csv",
+    col_types = cols(
+        year = col_integer(),
+        sex = col_factor(levels = sex_factor_levels()),
+        member = col_logical(),
+        covered = col_logical(),
+        age = col_integer(),
+        age_group = col_factor(levels = age_group_factor_levels()),
+        race = col_factor(race_factor_levels()),
+        hisp = col_logical(),
+        educ = col_factor(levels = education_factor_levels()),
+        citizen = col_factor(levels = citizenship_factor_levels()),
+        state = col_factor(levels = state_factor_levels())
+    )
+)
+
+# Define some useful globals ------------------------------------------------------------------
+
+min_year <- as.double(min(nurses$year, na.rm = TRUE))
+max_year <- as.double(max(nurses$year, na.rm = TRUE))
---
+++ server.R
@@ -0,0 +1,214 @@
+# File for defining the server logic, as well as supporting functions.
+
+library(shiny)
+library(usmap)
+library(ggplot2)
+library(dplyr)
+
+source("factor_levels.R")
+
+# Helper functions ----------------------------------------------------------------------------
+
+# Return union membership or union contract coverage trend data.
+#   nurses_subset
+#       Subset of the data selected by the user.
+#   group_var
+#       Variable to group by as a string. ``none'' if no grouping.
+#   type
+#       A string, either ``membership'' (for union membership rate) or ``coverage'' (for union
+#       contract coverage rate).
+trend_data <- function(nurses_subset, group_var, type) {
+    trend_data <- nurses_subset %>% group_by(year)
+    
+    group_var <- as.symbol(group_var)
+    if (group_var != "none") {
+        trend_data <- trend_data %>%
+            group_by(.dots = group_var, add = TRUE)
+    }
+    
+    if (type == "membership") {
+        trend_data <- trend_data %>%
+            summarize(
+                prop = mean(member, na.rm = TRUE),
+                n = n()
+            )
+    } else if (type == "coverage") {
+        trend_data <- trend_data %>%
+            summarize(
+                prop = mean(covered, na.rm = TRUE),
+                n = n()
+            )
+    } else {
+        stop("Type must be either ``membership'' or ``coverage''.")
+    }
+    
+    trend_data
+}
+
+# Plot union membership or union contract coverage over time.
+#   nurses_subset
+#       Subset of the data selected by the user.
+#   group_var
+#       Variable to group by as a string. ``none'' if no grouping.
+#   type
+#       A string, either ``membership'' (for union membership rate) or ``coverage'' (for union
+#       contract coverage rate).
+# TODO: Plot axis labels, etc. + plot styling.
+trend_plot <- function(nurses_subset, group_var, type) {
+    trend_data <- trend_data(nurses_subset, group_var, type)
+    
+    group_var <- as.symbol(group_var)
+    # TODO: aes() vs aes_()?
+    if (group_var != "none") {
+        p <- ggplot(trend_data,
+                    aes_(quote(year), quote(prop), color = group_var))
+    } else {
+        p <- ggplot(trend_data, aes(year, prop))
+    }
+    
+    p + geom_line() + expand_limits(y = 0)
+}
+
+# Return state-level union membership or union contract coverage.
+#   nurses_subset
+#       Subset of the data selected by the user.
+#   type
+#       A string, either ``membership'' (for union membership rate) or ``coverage'' (for union
+#       contract coverage rate).
+state_data <- function(nurses_subset, type) {
+    state_data <- nurses_subset %>% group_by(state)
+    
+    if (type == "membership") {
+        state_data <- state_data %>%
+            summarize(
+                prop = mean(member, na.rm = TRUE),
+                n = n()
+            )
+    } else if (type == "coverage") {
+        state_data <- state_data %>%
+            summarize(
+                prop = mean(covered, na.rm = TRUE),
+                n = n()
+            )
+    } else {
+        stop("Type must be either ``membership'' or ``coverage''.")
+    }
+    
+    state_data
+}
+
+# Plot a chloropleth map showing state-level union membership or union contract coverage.
+#   nurses_subset
+#       Subset of the data selected by the user.
+#   selected_states_only
+#       If TRUE, display only the states selected by the user on the map; otherwise, show all
+#       states.
+#   type
+#       A string, either ``membership'' (for union membership rate) or ``coverage'' (for union
+#       contract coverage rate).
+# TODO: Map styling, etc.
+state_map <- function(nurses_subset, selected_states_only, type) {
+    state_data <- state_data(nurses_subset, type)
+    
+    if (type == "membership") {
+        legend_name <- "Proportion members"
+    } else if (type == "coverage") {
+        legend_name <- "Proportion covered"
+    } else {
+        stop("Type must be either ``membership'' or ``coverage''.")
+    }
+    
+    # TODO: Am I doing this right?
+    states <- NULL
+    if (selected_states_only)
+        states <- state_data$state
+    
+    # TODO: Am I doing this right?
+    plot_usmap(data = state_data, value = "prop", include = states) +
+        scale_fill_continuous(name = legend_name, label = scales::comma) +
+        theme(legend.position = "right")
+}
+
+# Server logic --------------------------------------------------------------------------------
+
+server <- function(input, output) {
+    ## Handle data subset selection ##
+    
+    # Returns the subset of the nurses data selected by the user.
+    nurses_subset_selected <- reactive({
+        nurses %>%
+            filter(
+                year >= input$year_range[1], year <= input$year_range[2],
+                sex %in% input$sex_selection,
+                age_group %in% input$age_selection,
+                race %in% input$race_selection,
+                hisp %in% input$hisp_status_selection,
+                educ %in% input$educ_selection,
+                citizen %in% input$citizen_selection,
+                state %in% input$state_selection
+            )
+    })
+    
+    ## Trend plots and associated data tables ##
+    
+    # Renders the trend plot for union membership.
+    output$members_trend_plot <- renderPlot({
+        nurses_subset <- nurses_subset_selected()
+        group_var <- input$trends_group_var
+        trend_plot(nurses_subset, group_var, type = "membership")
+    })
+    
+    # Renders the trend plot for union contract coverage.
+    output$coverage_trend_plot <- renderPlot({
+        nurses_subset <- nurses_subset_selected()
+        group_var <- input$trends_group_var
+        trend_plot(nurses_subset, group_var, type = "coverage")
+    })
+    
+    # Renders the data table showing the union membership trend data.
+    output$membership_trend_data <- renderDataTable({
+        nurses_subset <- nurses_subset_selected()
+        group_var <- input$trends_group_var
+        trend_data(nurses_subset, group_var, type = "membership")
+    })
+    
+    # Renders the data table showing the union contract coverage trend data.
+    output$coverage_trend_data <- renderDataTable({
+        nurses_subset <- nurses_subset_selected()
+        group_var <- input$trends_group_var
+        trend_data(nurses_subset, group_var, type = "coverage")
+    })
+    
+    ## Maps and associated data tables ##
+    
+    # Renders the map showing union membership at the state-level.
+    output$membership_state_map <- renderPlot({
+        nurses_subset <- nurses_subset_selected()
+        selected_states_only <- input$selected_states_only
+        state_map(nurses_subset, selected_states_only, type = "membership")
+    })
+    
+    # Renders the map showing union contract coverage at the state-level.
+    output$coverage_state_map <- renderPlot({
+        nurses_subset <- nurses_subset_selected()
+        selected_states_only <- input$selected_states_only
+        state_map(nurses_subset, selected_states_only, type = "coverage")
+    })
+    
+    # Renders the data table showing the state-level union membership data.
+    output$membership_state_data <- renderDataTable({
+        nurses_subset <- nurses_subset_selected()
+        state_data(nurses_subset, type = "membership")
+    })
+    
+    # Renders the data table showing the state-level union contract coverage data.
+    output$coverage_state_data <- renderDataTable({
+        nurses_subset <- nurses_subset_selected()
+        state_data(nurses_subset, type = "coverage")
+    })
+    
+    ## Data table showing entire data selection ##
+    
+    # Renders the data table showing the subset of the nurses data selected by the user.
+    output$nurses_subset_table <- renderDataTable(nurses_subset_selected())
+}

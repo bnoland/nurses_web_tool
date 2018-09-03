7f7a4d36d3779483756cebcd98872cc37efc8a00
Benjamin Noland <benjaminnoland93@gmail.com>
2018-09-03 19:02:48 -0400

    Started filling in plot tests + changed some function names in test_data.R

--- tests/test_data.R
+++ tests/test_data.R
@@ -7,7 +7,7 @@ source("../server.R", chdir = TRUE)
 # TODO: Test on random subset of the full dataset?
 
 test_that("Trend grouped data", {
-    expect_grouped_data_equal <- function(nurses_subset, group_var, type) {
+    expect_data_equal <- function(nurses_subset, group_var, type) {
         actual_data <- trend_data(
             nurses_subset = nurses_subset,
             group_var = group_var,
@@ -36,27 +36,27 @@ test_that("Trend grouped data", {
         expect_equal(actual_data, expected_data)
     }
     
-    expect_grouped_data_equal(nurses, "none", type = "membership")
-    expect_grouped_data_equal(nurses, "sex", type = "membership")
-    expect_grouped_data_equal(nurses, "age_group", type = "membership")
-    expect_grouped_data_equal(nurses, "race", type = "membership")
-    expect_grouped_data_equal(nurses, "hisp", type = "membership")
-    expect_grouped_data_equal(nurses, "educ", type = "membership")
-    expect_grouped_data_equal(nurses, "citizen", type = "membership")
-    expect_grouped_data_equal(nurses, "state", type = "membership")
+    expect_data_equal(nurses, "none", type = "membership")
+    expect_data_equal(nurses, "sex", type = "membership")
+    expect_data_equal(nurses, "age_group", type = "membership")
+    expect_data_equal(nurses, "race", type = "membership")
+    expect_data_equal(nurses, "hisp", type = "membership")
+    expect_data_equal(nurses, "educ", type = "membership")
+    expect_data_equal(nurses, "citizen", type = "membership")
+    expect_data_equal(nurses, "state", type = "membership")
     
-    expect_grouped_data_equal(nurses, "none", type = "coverage")
-    expect_grouped_data_equal(nurses, "sex", type = "coverage")
-    expect_grouped_data_equal(nurses, "age_group", type = "coverage")
-    expect_grouped_data_equal(nurses, "race", type = "coverage")
-    expect_grouped_data_equal(nurses, "hisp", type = "coverage")
-    expect_grouped_data_equal(nurses, "educ", type = "coverage")
-    expect_grouped_data_equal(nurses, "citizen", type = "coverage")
-    expect_grouped_data_equal(nurses, "state", type = "coverage")
+    expect_data_equal(nurses, "none", type = "coverage")
+    expect_data_equal(nurses, "sex", type = "coverage")
+    expect_data_equal(nurses, "age_group", type = "coverage")
+    expect_data_equal(nurses, "race", type = "coverage")
+    expect_data_equal(nurses, "hisp", type = "coverage")
+    expect_data_equal(nurses, "educ", type = "coverage")
+    expect_data_equal(nurses, "citizen", type = "coverage")
+    expect_data_equal(nurses, "state", type = "coverage")
 })
 
 test_that("Trend difference data", {
-    expect_diff_data_equal <- function(nurses_subset, diff_var, diff_levels, type) {
+    expect_data_equal <- function(nurses_subset, diff_var, diff_levels, type) {
         actual_data <- trend_data(
             nurses_subset = nurses_subset,
             plot_diff = TRUE,
@@ -97,12 +97,12 @@ test_that("Trend difference data", {
     }
     
     # TODO: Test random levels?
-    expect_diff_data_equal(nurses, "sex", c("male", "female"), type = "membership")
-    expect_diff_data_equal(nurses, "sex", c("male", "female"), type = "coverage")
+    expect_data_equal(nurses, "sex", c("male", "female"), type = "membership")
+    expect_data_equal(nurses, "sex", c("male", "female"), type = "coverage")
 })
 
 test_that("State data", {
-    expect_state_data_equal <- function(nurses_subset, type) {
+    expect_data_equal <- function(nurses_subset, type) {
         actual_data <- state_data(nurses_subset, type)
         
         if (type == "membership") {
@@ -124,6 +124,6 @@ test_that("State data", {
         expect_equal(actual_data, expected_data)
     }
     
-    expect_state_data_equal(nurses, type = "membership")
-    expect_state_data_equal(nurses, type = "coverage")
+    expect_data_equal(nurses, type = "membership")
+    expect_data_equal(nurses, type = "coverage")
 })
--- tests/test_plots.R
+++ tests/test_plots.R
@@ -1,6 +1,22 @@
 context("Plot generation")
 
 test_that("Trend grouped plots", {
+    expect_data_equal <- function(nurses_subset, group_var, type) {
+        expected_data <- trend_data(
+            nurses_subset = nurses_subset,
+            group_var = group_var,
+            type = type
+        )
+        
+        p <- trend_plot(
+            nurses_subset = nurses_subset,
+            group_var = group_var,
+            type = type
+        )
+        
+        expect_equal(p$data, expected_data)
+    }
+    
 })
 
 test_that("Trend difference plots", {

15cfea53be461efcf5037eb33e57ddfa482e2380
Benjamin Noland <benjaminnoland93@gmail.com>
2019-01-07 22:43:08 -0800

    Fiddling with shinytest

--- tests/mytest.R
+++ tests/mytest.R
@@ -4,3 +4,7 @@ app$snapshotInit("mytest")
 #app$setInputs(selection_panels = "Sex")
 app$setInputs(sex_selection = "Female")
 app$snapshot()
+
+vals <- app$getAllValues()
+#str(vals)
+print(vals$input$educ_selection)
--- tests/mytest2.R
+++ tests/mytest2.R
@@ -1,7 +1,4 @@
 app <- ShinyDriver$new("../")
 app$snapshotInit("mytest2")
 
-#app$setInputs(selection_panels = "States")
-app$setInputs(state_selection = character(0))
-app$setInputs(state_selection = "CT")
 app$snapshot()
